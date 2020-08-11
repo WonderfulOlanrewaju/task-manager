921c1b021751cd983b56a98e7fb1cab6878ed7d0
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-08-12 00:46:43 +0100

    ignores

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,8 @@
 import {app} from './app';
+import dotenv from 'dotenv';
+dotenv.config();
 
-let port : number | string = process.env.PORT  || 9090
-app.listen(9090, ()=> console.log(`API running on port ${port}`));
+let port : number | string = process.env.PORT  || 9090;
+
+process.env.NODE_ENV ? 
+app.listen(9090, ()=> console.log(`API running on port ${port}`)) : null
