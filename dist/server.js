2354ee0592c52e8dfd2026d30fb49b92914bfd2a
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-08-12 01:01:49 +0100

    sequelize init

--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 let port = process.env.PORT || 9090;
-app_1.app.listen(9090, () => console.log(`API running on port ${port}`));
+process.env.NODE_ENV ?
+    app_1.app.listen(9090, () => console.log(`API running on port ${port}`)) : null;
 //# sourceMappingURL=server.js.map
